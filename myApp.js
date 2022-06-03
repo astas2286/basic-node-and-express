1ec9bef4aab38377a4aa87395ba842392ded8d60
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-06-03 14:57:05 +0300

    Add message via send method

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+app.get("/", function (req, res) {
+    res.send("Hello Express");
+});
 
 
 
@@ -36,4 +39,5 @@ console.log("Hello World");
 
 
 
- module.exports = app;
+
+module.exports = app;
