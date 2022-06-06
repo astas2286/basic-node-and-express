a5dbad66a102ef2ac9375cc7665a39166bbcfcd2
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-06-06 11:58:53 +0300

    Add timestamp midleware

--- myApp.js
+++ myApp.js
@@ -7,10 +7,19 @@ console.log("Hello World")
 //     res.send("Hello Express");
 // });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     console.log(`${req.method} ${req.path} -${req.ip}`);
     next();
-  })
+})
+
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.send({
+        time: req.time
+    });
+});
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
